591ca4cc3dba94d010f1b74d6c4c46ef9e81bdd5
seijiro <modeverv@gmail.com>
2022-04-30 23:36:33 +0900

    2022-04-30 23:36:32

--- android/app/src/main/java/co/appbrewery/flash_chat/MainActivity.java
+++ android/app/src/main/java/co/appbrewery/flash_chat/MainActivity.java
@@ -1,11 +1,23 @@
 package co.appbrewery.flash_chat;
 
+import androidx.annotation.NonNull;
+
 import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
-//  @Override
-//  protected void onCreate(Bundle savedInstanceState) {
-//    super.onCreate(savedInstanceState);
-//    GeneratedPluginRegistrant.registerWith(this);
-//  }
+    private static final String CHANNEL = "samples.flutter.dev/battery";
+
+    @Override
+    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+        new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL)
+                .setMethodCallHandler(
+                        (call, result) -> {
+                            // Your existing code
+                        }
+                );
+    }
 }
